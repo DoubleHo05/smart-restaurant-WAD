2c2d43d40a9680d4b1667a863a3eaf976cd1e14f
23120040 <anhminsen111@gmail.com>
2026-01-07 10:58:46 +0700

    Implement for login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,53 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 import TableManagement from "./pages/TableManagement";
 import Menu from "./pages/Menu";
 import CategoriesManagement from "./pages/CategoriesManagement";
 import ModifiersManagement from "./pages/ModifiersManagement";
 import MenuItemsManagement from "./pages/MenuItemsManagement";
+import AdminLayout from "./components/AdminLayout";
+import AdminLogin from "./pages/AdminLogin";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<TableManagement />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/categories" element={<CategoriesManagement />} />
-        <Route path="/modifiers" element={<ModifiersManagement />} />
-        <Route path="/items" element={<MenuItemsManagement />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/admin/login" element={<AdminLogin />} />
+
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute roles={["admin", "super_admin"]}>
+                <AdminLayout>
+                  <Routes>
+                    <Route
+                      path="/"
+                      // element={<Navigate to="/dashboard" replace />}
+                    />
+                    {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+                    <Route path="/tables" element={<TableManagement />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route
+                      path="/categories"
+                      element={<CategoriesManagement />}
+                    />
+                    <Route
+                      path="/modifiers"
+                      element={<ModifiersManagement />}
+                    />
+                    <Route path="/items" element={<MenuItemsManagement />} />
+                  </Routes>
+                </AdminLayout>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
